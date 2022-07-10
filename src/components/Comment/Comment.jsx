142fdc91ed0990ecb310d4f7847251cf2bff71a9
vlcoscar8 <vlcoscar8@gmail.com>
2022-07-10 19:07:55 +0200

    comments styles right and left flow bug updated

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Grid } from "@mui/material";
+import React, { useContext } from "react";
+import { AuthContext } from "../../state/context/authContext";
 
 const Comment = ({ comment }) => {
-    return <div>{comment.content}</div>;
+    const { userData } = useContext(AuthContext);
+
+    return (
+        <>
+            {userData && (
+                <Grid
+                    container
+                    spacing={0}
+                    direction="row"
+                    alignItems="center"
+                    justifyContent={
+                        comment.from[0].username === userData.username
+                            ? "right"
+                            : "left"
+                    }
+                    gap="1rem"
+                >
+                    {comment.content}
+                </Grid>
+            )}
+        </>
+    );
 };
 
 export default Comment;
