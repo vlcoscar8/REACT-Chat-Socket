44c1ae4751d446760ffa748392509ca22e8ce7e2
vlcoscar8 <vlcoscar8@gmail.com>
2022-07-10 12:22:51 +0200

    remove user from chat with redux updated

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,8 +1,15 @@
 import { Button } from "@mui/joy";
 import React from "react";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { useDispatch } from "react-redux/es/hooks/useDispatch";
+import { setReduxStateDeleteUserFromChat } from "../../state/redux/actions/usersActions";
 
 const User = ({ user }) => {
-    const removeUser = () => {};
+    const dispatch = useDispatch();
+
+    const removeUser = async () => {
+        dispatch(setReduxStateDeleteUserFromChat(user));
+    };
     return <Button onClick={removeUser}>{user.username}</Button>;
 };
 
