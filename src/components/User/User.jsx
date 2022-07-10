5df6651c041c1768a6e7b95d4b531815a8ac4b25
vlcoscar8 <vlcoscar8@gmail.com>
2022-07-10 12:28:35 +0200

    remove user from chat in data base

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,13 +1,27 @@
 import { Button } from "@mui/joy";
 import React from "react";
-import { useSelector } from "react-redux/es/hooks/useSelector";
+import { environment } from "../../environment/environment";
 import { useDispatch } from "react-redux/es/hooks/useDispatch";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 import { setReduxStateDeleteUserFromChat } from "../../state/redux/actions/usersActions";
 
 const User = ({ user }) => {
     const dispatch = useDispatch();
+    const { chatActive } = useSelector((state) => state.chatActive);
 
     const removeUser = async () => {
+        const bodyRequest = {
+            username: user.username,
+        };
+        const requestOptions = {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(bodyRequest),
+        };
+        await fetch(
+            `${environment.API_URL}/chat/remove/user/${chatActive.id}`,
+            requestOptions
+        );
         dispatch(setReduxStateDeleteUserFromChat(user));
     };
     return <Button onClick={removeUser}>{user.username}</Button>;
